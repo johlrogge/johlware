ae0ef9b973f780a67da2945a55cc3c1fc79a08a2
Joakim Ohlrogge <joakim.ohlrogge@agical.com>
2016-11-09 21:40:04 +0100

    read input and indicate output

--- src/main.rs
+++ src/main.rs
@@ -5,25 +5,24 @@
 extern crate teensy3;
 
 use teensy3::bindings;
-use teensy3::serial::Serial;
 
 #[no_mangle]
 pub unsafe extern fn main() {
     // Blink Loop
-
     bindings::pinMode(13, bindings::OUTPUT as u8);
     bindings::digitalWrite(13, bindings::LOW as u8);
-    let mut ser = Serial{};
 
-    loop {
-        // Show we are alive
-        alive();
+    bindings::pinMode(23, bindings::INPUT as u8);
 
-        // If the serial write fails, we will halt (no more alive blinks)
-        hello(&ser).unwrap();
+    bindings::pinMode(0, bindings::OUTPUT as u8);
+    bindings::digitalWrite(0, bindings::HIGH as u8);
 
-        // Don't spam the console
-        bindings::delay(1000);
+    bindings::pinMode(11, bindings::OUTPUT as u8);
+    bindings::digitalWrite(11, bindings::HIGH as u8);
+
+  loop {
+      bindings::digitalWrite(11, bindings::digitalRead (23));
+      bindings::delay(10);
     }
 }
 
@@ -37,10 +36,4 @@ pub unsafe fn alive() {
         bindings::digitalWrite(13, bindings::LOW as u8);
         bindings::delay(200);
     }
-}
-
-/// Send a message over the USB Serial port
-pub fn hello(ser: &Serial) -> Result<(),()> {
-    let msg = "Hello Teensy Rusty World!\n\r";
-    ser.write_bytes(msg.as_bytes())
 }
