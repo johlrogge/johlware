ec296495d951f63800b6917f020b2e0fec109a8e
Joakim Ohlrogge <joakim.ohlrogge@agical.com>
2016-11-20 17:45:47 +0100

    setup test bed for scanning. Also downloaded keyboard example for reference

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,24 @@ use teensy3::bindings;
 
 #[no_mangle]
 pub unsafe extern fn main() {
-    // Blink Loop
-    bindings::pinMode(13, bindings::OUTPUT as u8);
-    bindings::digitalWrite(13, bindings::LOW as u8);
 
-    bindings::pinMode(23, bindings::INPUT as u8);
 
-    bindings::pinMode(0, bindings::OUTPUT as u8);
-    bindings::digitalWrite(0, bindings::HIGH as u8);
+    for pin in 18..21 {
+        bindings::pinMode(pin, bindings::INPUT_PULLUP as u8);
+    }
 
-    bindings::pinMode(11, bindings::OUTPUT as u8);
-    bindings::digitalWrite(11, bindings::HIGH as u8);
+    for pin in 21..24 {
+        bindings::pinMode(pin, bindings::OUTPUT as u8);
+        bindings::digitalWrite(pin, bindings::LOW as u8);
+    }
+
+    for pin in 0..9 {
+        bindings::pinMode (pin, bindings::OUTPUT as u8);
+        bindings::digitalWrite (pin, bindings::HIGH as u8);
+    }
 
   loop {
-      bindings::digitalWrite(11, bindings::digitalRead (23));
+      bindings::digitalWrite(0, bindings::digitalRead (18));
       bindings::delay(10);
     }
 }
